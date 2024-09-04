f8afc6557e988ab29bb48fff7e49d85dfa7f6f0d
sofyan <sofyanardiansyah888@gmail.com>
2024-09-04 13:10:40 +0800

    fix: change logic empty data

--- src/components/empty-data.tsx
+++ src/components/empty-data.tsx
@@ -1,12 +1,12 @@
 import {Empty} from "antd";
 import React from "react";
 
-export default function EmptyData({data,fullscreen = false}: { data: any[] | undefined, fullscreen?: boolean }) {
-    return data?.length === 0 || data === undefined &&
+export default function EmptyData({data, fullscreen = false}: { data: any[] | undefined, fullscreen?: boolean }) {
+    return data?.length === 0 || data === undefined ?
         <div className={`flex items-center ${fullscreen ? "h-[80vh]" : ""}`}>
-        <Empty
-            description={"Data Kosong"}
-            className={"mx-auto size-24 my-4"}
-        />
-        </div>
+            <Empty
+                description={"Data Kosong"}
+                className={"mx-auto size-24 my-4"}
+            />
+        </div> : <></>
 }
